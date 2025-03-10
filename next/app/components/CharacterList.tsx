095fd5b1ee92524a24b3c443dd1efc21edf9b682
Adam Kumlin <kumlinadam99@gmail.com>
2025-03-10 10:49:34 +0100

    Fix data not formatting correctly

--- next/app/components/CharacterList.tsx
+++ next/app/components/CharacterList.tsx
@@ -4,11 +4,23 @@ import CharacterCard from "./CharacterCard";
 export default async function CharacterList() {
   const response = await fetch("http://swapi.dev/api/people");
   const data = await response.json();
+  const results: Character[] = [];
+
+  for (const entry of data.results) {
+    const character: Character = {
+      name: entry.name,
+      heightCm: entry.height,
+      massKg: entry.mass,
+      birthYear: entry.birth_year,
+    };
+
+    results.push(character);
+  }
 
   return (
     <div>
-      {data.results ? (
-        data.results.map((entry: Character, index: number) => (
+      {results ? (
+        results.map((entry: Character, index: number) => (
           <CharacterCard key={index} character={entry} />
         ))
       ) : (
